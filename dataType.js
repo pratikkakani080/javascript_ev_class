9b20851c352045787684e4a0db53b7ee05e0d1d5
Your Name <you@example.com>
2025-07-30 17:01:29 +0530

    Initial commit

---
+++ dataType.js
@@ -0,0 +1,40 @@
+// premitive
+    // 1. String
+        // Alphanumeric
+        const testVar = 'test'
+        console.log('' + 'test')
+        console.log("Uncle's" + ' ' + 'test')
+        console.log(`Template ${testVar} literal`)
+        console.log(5 + 4 + '4' + true);
+        const newStr = String()
+        console.log(newStr, 66666);
+
+    // 2. Number
+        // numeric 0 - 9
+        console.log( '9', 9);
+        const age = 44
+        console.log(Number(''));
+        // integer and float
+        // -2^53 to 2^53
+        // -9007199254740992 to 9007199254740992
+        
+    // 3. Boolean
+        // conditional data
+        // true, false
+        const isUserActive = true
+        console.log(Boolean(' '));
+        
+    // 4. Undefined
+        let undefinedVar = undefined
+        console.log(undefinedVar);
+        // can not read properties of undefined
+        
+    // 5. Null
+        let nullishValue = null
+
+    // 6. BigInt
+        Bigint() 
+        234n
+
+    // 7. Symbol
+// complex
---
+++ index.js
@@ -0,0 +1,50 @@
+// external js file
+
+console.log("this is external file loading...");
+
+// Variables
+let first_name = "pratik";
+first_name = "fosifsdiofj";
+console.log(first_name);
+
+var last_name = "kakani";
+var last_name = "dddd";
+console.log(last_name);
+
+const middle_name = "r";
+
+// Naming convention
+// things to avoid - test1, user1, user2, a1, a2
+// default keyword - const, var, let, name, value, number, array
+
+// casing
+// UPPERCASE
+// lowercase
+// camelCaseLetter
+// snake_case
+
+const fname = "john";
+const age = 55;
+const hobbies = ["to watch movies", "reading"];
+const user = {
+  fname: "john",
+  age: 55,
+  hobbies: ["to watch movies", "reading"],
+};
+const users = [
+  {
+    fname: "john",
+    age: 55,
+    hobbies: ["to watch movies", "reading"],
+  },
+  {
+    fname: "john2",
+    age: 53,
+    hobbies: ["to watch movies", "reading"],
+  },
+  {
+    fname: "john3",
+    age: 55,
+    hobbies: ["reading"],
+  },
+];
