281bc2a4fc634e646e8d9601e4b6d90e2a569f6e
Pratik <pratik080.rejoice@gmail.com>
2025-08-01 11:24:38 +0530

    methods: string

--- dataType.js
+++ dataType.js
@@ -2,18 +2,43 @@
     // 1. String
         // Alphanumeric
         const testVar = 'test'
-        console.log('' + 'test')
-        console.log("Uncle's" + ' ' + 'test')
-        console.log(`Template ${testVar} literal`)
-        console.log(5 + 4 + '4' + true);
+        // console.log('' + 'test')
+        // console.log("Uncle's" + ' ' + 'test')
+        // console.log(`Template ${testVar} literal`)
+        // console.log(5 + 4 + '4' + true);
         const newStr = String()
-        console.log(newStr, 66666);
+        // console.log(newStr, 66666);
+        const stringToCheckMethods = '   This string is generated to test all the methods \uD800    '
+        console.log(stringToCheckMethods.length);
+        console.log(stringToCheckMethods.charAt(30));
+        console.log(stringToCheckMethods.charCodeAt(30));
+        console.log(stringToCheckMethods.codePointAt(30));
+        console.log(stringToCheckMethods.concat(' ', 'this is merged', ' ', 'string'));
+        console.log(stringToCheckMethods + ' ' + 'this is merged' + ' ' + 'string');
+        console.log(stringToCheckMethods.at(30));
+        console.log(stringToCheckMethods[30]);
+        console.log(stringToCheckMethods.slice(15, 24));
+        console.log(stringToCheckMethods.slice(-15, -3));
+        console.log(stringToCheckMethods.substring(15, 24));
+        console.log(stringToCheckMethods.toUpperCase());
+        console.log(stringToCheckMethods.toLowerCase());
+        console.log(stringToCheckMethods.isWellFormed());
+        console.log(stringToCheckMethods.toWellFormed());
+        console.log(stringToCheckMethods.trim());
+        console.log(stringToCheckMethods.trimStart());
+        console.log(stringToCheckMethods.trimEnd());
+        console.log(stringToCheckMethods.padStart(60, 0));
+        console.log(stringToCheckMethods.padEnd(60, 'T'));
+        console.log(stringToCheckMethods.repeat(4));
+        console.log(stringToCheckMethods.replace('t', 'A'));
+        console.log(stringToCheckMethods.replaceAll('t', 'A'));
+        console.log(stringToCheckMethods.split(' '));
 
     // 2. Number
         // numeric 0 - 9
-        console.log( '9', 9);
+        // console.log( '9', 9);
         const age = 44
-        console.log(Number(''));
+        // console.log(Number(''));
         // integer and float
         // -2^53 to 2^53
         // -9007199254740992 to 9007199254740992
@@ -22,11 +47,11 @@
         // conditional data
         // true, false
         const isUserActive = true
-        console.log(Boolean(' '));
+        // console.log(Boolean(' '));
         
     // 4. Undefined
         let undefinedVar = undefined
-        console.log(undefinedVar);
+        // console.log(undefinedVar);
         // can not read properties of undefined
         
     // 5. Null
@@ -37,8 +62,8 @@
         234n
 
     // 7. Symbol
-        console.log(Symbol());
-        console.log(Symbol() === Symbol());
+        // console.log(Symbol());
+        // console.log(Symbol() === Symbol());
         
 // complex datatype
     // Object
@@ -53,7 +78,7 @@
             },
             testArr: ['testData1', 'testData2']
          }
-         console.log(photoFrame.testObj.testKey)
+        //  console.log(photoFrame.testObj.testKey)
     // Array
          const photsList = [
             { 
@@ -98,18 +123,18 @@
             testArr: ['testData1', 'testData2']
          }
          ]
-         console.log(photsList);
+        //  console.log(photsList);
          
     // Functions
          function myFirstFun() {
-            console.log('this is function running');
+            // console.log('this is function running');
          }
 
          myFirstFun()
 
          // Arrow function
          const myFirstArrowFun = () => {
-            console.log('this is arrow function running');
+            // console.log('this is arrow function running');
          }
 
          myFirstArrowFun()
