458c0315034f544505183daf1c5e5dea68a3c9fa
Pratik <pratik080.rejoice@gmail.com>
2025-08-05 17:37:12 +0530

    add: arrray methods

--- dataType.js
+++ dataType.js
@@ -93,15 +93,15 @@
             testArr: ['testData1', 'testData2']
          }
         //  console.log(photoFrame.testObj.testKey)
-        console.log(Object.assign({}, {nameOfGod: 'Shiva'}, {num: 666}))
+        // console.log(Object.assign({}, {nameOfGod: 'Shiva'}, {num: 666}))
         delete photoFrame.testObj
-        console.log(photoFrame);
-        console.log(JSON.stringify(photoFrame))
+        // console.log(photoFrame);
+        // console.log(JSON.stringify(photoFrame))
         document.writeln(JSON.stringify(photoFrame))
-        console.log(JSON.parse(JSON.stringify(photoFrame)))
-        console.log(Object.keys(photoFrame))
-        console.log(Object.values(photoFrame))
-        console.log(Object.entries(photoFrame))
+        // console.log(JSON.parse(JSON.stringify(photoFrame)))
+        // console.log(Object.keys(photoFrame))
+        // console.log(Object.values(photoFrame))
+        // console.log(Object.entries(photoFrame))
         
     // Array
          const photsList = [
@@ -142,11 +142,52 @@
             nameOfGod: 'Shiva',
             isLighting: true,
             height: '12in',
-            width: '8in',
+            width1: '8in',
+            width3: '8in',
+            width4: '8in',
+            width5: '8in',
+            width6: '8in',
+            width7: '8in',
+            width8: '8in',
+            width9: '8in',
+            width23: '8in',
+            width54: '8in',
+            width65: '8in',
+            width234: '8in',
+            width2234: '8in',
+            width333: '8in',
+            width22: '8in',
+            width11: '8in',
+            width55: '8in',
+            width66: '8in',
+            width2222: '8in',
+            widt33h: '8in',
+            widt4h: '8in',
+            wid55th: '8in',
+            wid6th: '8in',
+            wi6dth: '9in',
            
             testArr: ['testData1', 'testData2']
          }
          ]
+         const arrOfNum = [3,4,5,6,7,8]
+        //  console.log(photsList.length);
+        //  console.log(arrOfNum.toString());
+        //  document.writeln(arrOfNum)
+        //  console.log(photsList.at(3));
+        //  console.log(photsList[0]);
+        //  console.log(arrOfNum.join(' | '));
+        //  console.log(photsList.pop()) // removes last element of the array
+        //  console.log(photsList.push({nameOfGod: 'Ganapati'})) // adds one element at the last index of the array
+        //  console.log(photsList.shift());
+        //  console.log(photsList.unshift({nameOfGod: 'Bhavani'}));
+        //  delete photsList[1] // do not use this
+         
+        //  console.log(photsList.concat([{nameOfGod: 'Sarasvati'}, {nameOfGod: 'Ambe ma'}, {nameOfGod: 'Krishna'}]));
+        //  console.log(photsList.copyWithin(2, 0, 1))
+        //  console.log(photsList.slice(1, 3));
+        //  console.log(photsList.toSpliced(1, 2));
+        //  console.log(photsList.splice(1, 2));
         //  console.log(photsList);
          
     // Functions
