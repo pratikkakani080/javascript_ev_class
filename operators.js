9907cb686e169421791b6a01854d9bea27ca3c9b
Pratik <pratik080.rejoice@gmail.com>
2025-08-11 17:05:08 +0530

    add: logical operators

--- operators.js
+++ operators.js
@@ -2,4 +2,13 @@
 // condition ? true statement : false statement
 
 const a = 12
-console.log('' ? 'true statement' : 'false statement');
+// console.log('' ? 'true statement' : 'false statement');
+
+
+// And operator &&
+let color = 'blue'
+let size = 24
+
+console.log(color !== 'red' && size > 30 ? 'true statement' : 'false statement');
+console.log(color !== 'red' || (size > 30 && size < 50) ? 'true statement' : 'false statement');
+
